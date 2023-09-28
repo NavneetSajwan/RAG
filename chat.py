1cd3fbca5c610d7b66ce3d57929b026af5ebb454
NavneetSajwan <navneetsinghsajwan@gmail.com>
2023-09-28 17:24:19 +0530

    created and tested chat script

---
+++ chat.py
@@ -0,0 +1,39 @@
+
+from langchain.schema import SystemMessage, HumanMessage, AIMessage
+from load_index import chat, vectorstore
+import warnings
+warnings.filterwarnings("ignore")
+
+def augment_prompt(query: str):
+    results = vectorstore.similarity_search(query, k=3)
+    source_knowledge = "\n".join([x.page_content for x in results])
+    augment_prompt = f""" Using the context below, answer the query.
+
+context: {source_knowledge}
+
+query: {query}
+"""
+    return augment_prompt
+
+def chat_me(messages):
+    print("Ask anything about llama2\n")
+    while(True):
+        query = input()
+        print(f"You: {query} \n")
+        if query.lower()=="bye":
+            return
+        prompt = augment_prompt(query)
+        messages.append(HumanMessage(content=prompt))
+        res = chat(messages)
+        print("AI: ", res.content, "\n")
+        messages.append(res)
+
+if __name__ == "__main__":
+    query = "how is llama 2 better than other llms?"
+    messages = [
+        SystemMessage(content="You are a helpful assistant. Your name is Friday"),
+        HumanMessage(content= "Hi Friday, how are you doing today?"),
+        SystemMessage(content="Hi, I'm great thank you. How can I help you?")
+    ]
+    out = chat_me(messages)
+    
